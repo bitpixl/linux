ef188e02e533daabf66ff206faf48178accd5e6f
bitpixl <29624712+bitpixl@users.noreply.github.com>
2021-04-22 10:26:31 +0000

    Update fd_min.sh

--- fd_min.sh
+++ fd_min.sh
@@ -6,14 +6,15 @@ if [[ `id -u` != 0 ]]; then
 fi
 
 # update system files before installing desktop enviroment
-dnf update -y
+# dnf update -y
 
 # install minimal kde plasma desktop
+dnf install -y xorg-x11-utils xdg-user-dirs plasma-desktop sddm sddm-kcm sddm-breeze breeze-gtk kde-gtk-config kde-settings-pulseaudio kde-style-breeze ksysguard plasma-breeze plasma-drkonqi plasma-nm plasma-nm-openvpn plasma-pa plasma-user-manager
 # dnf install -y plasma-desktop sddm bluedevil breeze-gtk breeze-icon-theme cups-pk-helper gnome-keyring-pam kcm_systemd kde-gtk-config 
 # dnf install -y kde-partitionmanager kde-print-manager kde-settings-pulseaudio kde-style-breeze kdegraphics-thumbnailers kdeplasma-addons 
 # dnf install -y kdialog kdnssd khotkeys kinfocenter kmenuedit kscreen kscreenlocker ksshaskpass ksysguard kwallet-pam kwalletmanager5 kwin 
 # dnf install -y pam-kwallet pinentry-qt plasma-breeze plasma-desktop-doc plasma-drkonqi plasma-nm plasma-nm-openvpn plasma-pa plasma-user-manager 
-# dnf install -y polkit-kde sddm-breeze sddm-kcm sddm-theme-breeze setroubleshoot sni-qt xorg-x11-drv-libinput
+# dnf install -y polkit-kde sddm-breeze sddm-kcm setroubleshoot sni-qt xorg-x11-drv-libinput
 
 # @"base-x" \
 # @"Common NetworkManager Submodules" \
